22e4fb045b3e8938010daf637bf2428d144dc618
Satyam26-09 <satyam.sharma.cse25@heritageit.edu.in>
2023-09-04 18:38:44 +0530

    Revert "added button and text to student dashboard"
    This reverts commit afabe9767a9065d527d48f182cb0aa9f77876378.

--- securesult/src/components/ResultContainer.js
+++ securesult/src/components/ResultContainer.js
@@ -3,13 +3,7 @@ import React from "react";
 const ResultContainer = () => {
   return (
     <div className="py-8 px-10">
-      <h1 className="text-gray-700 text-4xl mb-6">Hello, user name</h1>
-      <p className="text-gray-500 text-lg">
-        Your results will be shown here once uploaded..
-      </p>
-      <button className="bg-orange-500 text-white font-semibold mt-5 rounded-lg py-2 px-3">
-        Show Result
-      </button>
+      <h1 className="text-gray-700 text-4xl">Hello, user name</h1>
     </div>
   );
 };
